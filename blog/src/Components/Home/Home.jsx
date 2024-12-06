d3f1548259dea40e7b05728a9a212805722c7d23
dhruvalgondaliya <f342dhruval@gmail.com>
2024-12-06 17:52:06 +0530

    add mycity page

--- blog/src/Components/Home/Home.jsx
+++ blog/src/Components/Home/Home.jsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+
+      {/* hello testing  */}
+
     </div>
   )
 }
--- blog/src/Components/MyCity/MyCity.jsx
+++ blog/src/Components/MyCity/MyCity.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function MyCity() {
   return (
     <div>
+      {/* testing page */}
       <h1>My City Page ...</h1>
     </div>
   )
